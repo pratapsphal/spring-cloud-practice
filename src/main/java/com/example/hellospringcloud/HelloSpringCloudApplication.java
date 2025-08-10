5912d516685860a5e056ea83d3b5723bb2878394
Pratap <Pratap@host.docker.internal>
2025-08-10 21:12:11 +0530

    commit of Hello Spring Cloud project

---
+++ src/main/java/com/example/hellospringcloud/HelloSpringCloudApplication.java
@@ -0,0 +1,11 @@
+package com.example.hellospringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringCloudApplication.class, args);
+    }
+}
