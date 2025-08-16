97c00e8f110611ad76d9777bb3851ef667219454
Pratap <Pratap@host.docker.internal>
2025-08-16 17:19:26 +0530

    Merge branch 'master' of https://github.com/pratapsphal/spring-cloud-practice

--- src/main/java/com/example/hellospringcloud/HelloSpringCloudApplication.java
+++ src/main/java/com/example/hellospringcloud/HelloSpringCloudApplication.java
@@ -2,10 +2,14 @@ package com.example.hellospringcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // This annotation makes the application a Eureka client
 public class HelloSpringCloudApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloSpringCloudApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringCloudApplication.class, args);
+	}
+
 }
